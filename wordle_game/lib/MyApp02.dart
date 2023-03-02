896f412fc42c7e2fede1625237b82c4c6cdef627
d.hue <huedo10041999@gmail.com>
2023-03-02 22:56:28 +0900

    textField

--- wordle_game/lib/MyApp02.dart
+++ wordle_game/lib/MyApp02.dart
@@ -1,81 +1,68 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class MyApp02 extends StatefulWidget
-{
+class MyApp02 extends StatefulWidget {
   @override
   State<StatefulWidget> createState() {
     // TODO: implement createState
     return _MyAppState();
   }
 }
-class _MyAppState extends State<MyApp02> with WidgetsBindingObserver{
-  String _email = "";
-  final emailEditingController = TextEditingController();
 
-  @override
-  void initState() {
-    // TODO: implement initState
-    super.initState();
-    WidgetsBinding.instance.addObserver(this);
-    print('Run initstate');
-  }
-
-  @override
-  void dispose() {
-    // TODO: implement dispose
-    emailEditingController.dispose();
-    WidgetsBinding.instance.removeObserver(this);
-    print('Run dispose');
-  }
-
-  @override
-  void didChangeAppLifecycleState(AppLifecycleState state) {
-    // TODO: implement didChangeAppLifecycleState
-    super.didChangeAppLifecycleState(state);
-    if (state == AppLifecycleState.paused) {
-      print('background mode');
-    } else if (state == AppLifecycleState.resumed) {
-      print('foreground mode');
-    }
-  }
-
+class _MyAppState extends State<MyApp02> with WidgetsBindingObserver {
   @override
   Widget build(BuildContext context) {
     print('Run build');
+    final contextEdittingController = TextEditingController();
+    String context = "";
+    final amountEdittingController = TextEditingController();
+    int amount = 0;
+    //final GlobalKey<ScaffoldState> _scaffoldKey = new GlobalKey<ScaffoldState>()'';
     return MaterialApp(
-      title:'this is statefullWidget',
-      home:Scaffold(
+      title: 'this is statefullWidget',
+      home: Scaffold(
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              Container(
-                padding: EdgeInsets.symmetric(horizontal: 20,vertical: 20),
-                child: TextField(
-                  controller: emailEditingController,
-                  onChanged: (text){
-                    setState(() {
-                      _email = text;
-                    });
-                  },
-                  decoration: const InputDecoration(
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(20)
-                      )
-                    )
-                  ),
+              TextField(
+                controller: contextEdittingController,
+                onChanged: (text){
+                  setState(() {
+                    context = text;
+                  });
+                },
+                decoration: const InputDecoration(
+                  labelText: 'Context',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(20)),
+                  )
                 ),
+
               ),
-              Text(
-                _email,
-                style: const TextStyle(fontSize: 30,color: Colors.blueAccent),
+              TextField(
+                decoration: InputDecoration(labelText: 'Amount(money'),
+                controller: amountEdittingController,
+                onChanged: (text){
+                  setState(() {
+                    amount = int.tryParse(text) ?? 0;
+                  });
+                },
               ),
+              TextButton(
+                  child: Text('Insert Transaction'),
+                  style: TextButton.styleFrom(
+                    primary: Colors.blue,
+                    onSurface: Colors.red,
+                  ),
+                  onPressed: () {
+                     print('Context : $context , amount : $amount');
+                  }
+              )
             ],
           ),
         ),
-      )
-      
+      ),
     );
   }
 }
