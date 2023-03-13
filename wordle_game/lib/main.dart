5dd9692a632c9277bcc0dff09ab7f728a6cdd16a
d.hue <huedo10041999@gmail.com>
2023-03-13 22:18:35 +0900

    khởi tạo HomeScreen

--- wordle_game/lib/main.dart
+++ wordle_game/lib/main.dart
@@ -1,8 +1,23 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(const Center(
-      child: Text("Hello World!",
-          style: TextStyle(fontSize: 30),
-          textDirection: TextDirection.ltr),
-    )
-);
+void main()
+{
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Home Screen'),
+        ),
+        body: const Center(
+          child: Text('this is Home Screen'),
+        ),
+      ),
+    );
+  }
+}
